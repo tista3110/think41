58423a3541607ed60d072ca481ddded5f626edb5
tista3110 <tista644@gmail.com>
2025-08-04 13:44:03 +0530

    Delete Downloads/news_summarizer directory

--- Downloads/news_summarizer/app/utils/tts.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-import os
-
-def generate_hindi_audio(text, filename="hindi_summary.mp3"):
-    tts = gTTS(text=text, lang='hi')  # 'hi' for Hindi language
-    file_path = os.path.join("static", filename)
-    tts.save(file_path)
-    return file_path
